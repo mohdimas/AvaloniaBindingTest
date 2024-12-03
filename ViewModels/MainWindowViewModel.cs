521ffc7dab2b31eb2d8f3dd779e156e81e9b957c
Dimas Mohammad <mohammad.dimas.s@gmail.com>
2024-12-03 11:07:30 +0800

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace AvaloniaBindingTest.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+    public string Greeting => "Welcome to Avalonia!";
+
+    private bool _isChecked;
+
+    public bool IsChecked
+    {
+        get => _isChecked;
+        set
+        {
+            _isChecked = value;
+            OnPropertyChanged();
+        }
+    }
+    public RelayCommand RaisePropertyChangedCommand { get; }
+#pragma warning restore CA1822 // Mark members as static
+
+    public MainWindowViewModel()
+    {
+        RaisePropertyChangedCommand = new RelayCommand(ExecuteRaisePropertyChanged);
+    }
+
+    private void ExecuteRaisePropertyChanged()
+    {
+        OnPropertyChanged(nameof(IsChecked));
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaBindingTest.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
